32da5bde1fb686d8511992bd2a075aba2677afe5
Nghia Vi <nghia.minhvi@crossian.com>
2026-01-31 18:10:48 +0000

    chore: update next config
    Signed-off-by: Nghia Vi <nghia.minhvi@crossian.com>

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,32 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	/* config options here */
+	typedRoutes: true,
+	turbopack: {
+		rules: {
+			'*.svg': {
+				loaders: [
+					{
+						loader: '@svgr/webpack',
+						options: {
+							typescript: true,
+							svgo: false,
+							memo: true,
+						},
+					},
+				],
+				as: '*.js',
+			},
+		},
+	},
+	reactCompiler: true,
+	experimental: {
+		optimizePackageImports: ['@mantine/core', '@mantine/hooks', '@mantine/form', '@tabler/icons-react'],
+		turbopackFileSystemCacheForBuild: true,
+		cssChunking: 'strict',
+	},
+	output: process.env.OUTPUT as 'standalone' | 'export' | undefined,
 };
 
 export default nextConfig;
